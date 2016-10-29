362b049f7f69f520d0e18d7d328687665bd4876c
tono0123 <e080514@gmail.com>
2016-10-29 16:33:57 +0900

    Create Community

---
+++ spec/models/community_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe Community do
+	describe 'validation' do
+		let(:community) { Community.create(name: nil, description: nil) }
+		before do
+	    community
+	 	end
+
+	  it { community.name be_valid }
+	  it { community.description be_valid }
+	 end
+end
+
+
