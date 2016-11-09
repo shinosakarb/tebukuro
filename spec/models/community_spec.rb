868363b0c91053dfa5c93bc5ff81e6b4a0a39165
tono0123 <e080514@gmail.com>
2016-11-09 18:50:00 +0900

    Fix spec has_many is_expected

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Community, type: :model do
   describe 'validation' do
-    it { should have_many(:events) }
+    it { is_expected.to have_many(:events) }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:description) }
   end
