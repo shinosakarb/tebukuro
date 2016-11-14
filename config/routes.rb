eb2c2c70f996bc8cb81ce0643249080c67da5577
chimame <rito.tamata@gmail.com>
2016-11-14 20:27:59 +0900

    Fix review delete routing of non-create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   resources :communities, :defaults => { :format => 'json' } do
-      resources :events do
-        resources :tickets
-      end
-      resources :admins
-      resources :owners
-      resources :members
+      resources :events
     end
 end
