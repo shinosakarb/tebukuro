ee647f5b3bbc0e62376988389a08bef2df298002
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2016-11-17 23:10:32 +0900

    Fix failed test

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Community, type: :model do
   describe 'validation' do
     it { is_expected.to have_many(:events) }
-    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.not_to validate_presence_of(:name) }
     it { is_expected.not_to validate_presence_of(:description) }
   end
 end
