12d81b50895aefefe96d194b075a19577f9dd39f
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2016-11-17 23:39:17 +0900

    Fix failed test

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Community, type: :model do
   describe 'validation' do
     it { is_expected.to have_many(:events) }
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.not_to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:description) }
   end
 end
