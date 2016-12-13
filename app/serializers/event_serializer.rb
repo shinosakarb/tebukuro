2966b16e64b9649185d3e430d199d0388ff00f0e
yukihirop <te108186@gmail.com>
2016-12-14 01:40:03 +0900

    ADD event_detail_information

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,8 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :community_id
+  attributes :id, :name, :description, :community_id,
+             :invitation_starts_at, :invitation_ends_at,
+             :event_starts_at, :event_ends_at,
+             :number_of_accepted_participants, :cost
 
   # TODO: communitiesモデルが作成されたら、GETのURLをattributesに加えます。
 
---
+++ db/migrate/20161207133326_add_detail_to_events.rb
@@ -0,0 +1,12 @@
+class AddDetailToEvents < ActiveRecord::Migration[5.0]
+  def change
+    change_table :events do |t|
+      t.datetime :invitation_starts_at
+      t.datetime :invitation_ends_at
+      t.datetime :event_starts_at
+      t.datetime :event_ends_at
+      t.integer :number_of_accepted_participants
+      t.integer :cost
+    end
+  end
+end
