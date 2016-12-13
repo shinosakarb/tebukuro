36a30e8d0b446474e6aaca656a3a2fbd478358da
yukihirop <te108186@gmail.com>
2016-12-14 01:40:03 +0900

    ADD event_detail_information

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,37 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
+  validates :invitation_starts_at, presence:true
+  validates :invitation_ends_at, presence:true
+  validates :event_starts_at, presence:true
+  validates :event_ends_at, presence:true
+  validates :number_of_accepted_participants, presence: true
+  validates :cost, presence: true
+
+=begin
+
+  # TODO: date_validatorのgemを使うなら以下が有効になる
+
+  validates :invitation_starts_at, presence: true, date:{
+      after_or_equal_to: Date.today.to_time,
+      allow_blank: false
+  }
+
+  validates :invitation_ends_at, presence: true, date:{
+      after: :invitation_started_at,
+      allow_blank: true
+  }
+
+  validates :event_starts_at, presence: true, date:{
+      after: :invitation_ended_at,
+      allow_blank:false
+  }
+
+  validates :event_ends_at, presence: true, date:{
+      after: event_started_at,
+      allow_blank:false
+  }
+
+=end
+
 end
