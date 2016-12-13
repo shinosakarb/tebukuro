e9aec92dd9443ca7e72d7b3450a26afbcc5e5ef9
yukihirop <te108186@gmail.com>
2016-12-14 01:40:03 +0900

    ADD event_detail_information

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,5 +5,13 @@ describe Event do
     it { is_expected.to belong_to(:community) }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:invitation_starts_at)}
+    it { is_expected.to validate_presence_of(:invitation_ends_at)}
+    it { is_expected.to validate_presence_of(:event_starts_at)}
+    it { is_expected.to validate_presence_of(:event_ends_at)}
+    it { is_expected.to validate_presence_of(:number_of_accepted_participants)}
+    it { is_expected.to validate_presence_of(:cost)}
+
   end
+
 end
