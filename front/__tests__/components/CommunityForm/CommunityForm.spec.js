71b8f3d89819df7510e960962434263408d52e17
chimame <rito.tamata@gmail.com>
2016-12-25 22:29:17 +0900

    Move spec file for front

--- front/__tests__/components/CommunityForm/CommunityForm.spec.js
+++ front/__tests__/components/CommunityForm/CommunityForm.spec.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import CommunityForm from '../index'
+import React from 'react'
+import CommunityForm from '../../../client/components/CommunityForm'
 import renderer from 'react-test-renderer'
 
 describe('CommunityForm', () => {
