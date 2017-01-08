2edce9b1b436d0fddae44c2657ff2c9dbb197ba9
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2017-01-09 02:57:43 +0900

    Use EachValidator instead of Validator

---
+++ app/validators/datetime_less_validator.rb
@@ -0,0 +1,20 @@
+class DatetimeLessValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    before_datetime = record.send(options[:before])
+    target_attr     = record.class.human_attribute_name options[:before]
+
+    if before_datetime.blank?
+      error   = :required_before_datetime
+      message = "must not be nil #{target_attr}"
+    elsif (before_datetime - value).to_i < 0
+      error   = :less_than_datetime
+      message = "must be before #{target_attr}"
+    end
+
+    record.errors.add(
+      attribute, error,
+      target_attr: target_attr,
+      message:     message
+    ) if error.present?
+  end
+end
---
+++ spec/validators/datetime_less_validator_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe DatetimeLessValidator do
+  let(:dummy_class) do
+    Struct.new(:start_at, :end_at) do
+      include ActiveModel::Validations
+
+      validates :start_at,
+        datetime_less: {
+          before: :end_at
+        }
+
+      def self.name
+        'Dummy'
+      end
+    end
+  end
+
+  describe 'start_at' do
+    let(:base_datetime) { Time.current }
+
+    subject { dummy_class.new(start_at, end_at) }
+
+    context 'less than end_at' do
+      let(:start_at) { base_datetime }
+      let(:end_at)   { base_datetime.tomorrow }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'equal to end_at' do
+      let(:start_at) { base_datetime }
+      let(:end_at)   { base_datetime }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'greater than end_at' do
+      let(:start_at) { base_datetime }
+      let(:end_at)   { base_datetime.yesterday }
+
+      example do
+        is_expected.not_to be_valid
+        expect(subject.errors.messages).to include(
+          start_at: ['must be before End at']
+        )
+      end
+    end
+
+    context 'target nil' do
+      let(:start_at) { base_datetime }
+      let(:end_at)   { nil }
+
+      example do
+        is_expected.not_to be_valid
+        expect(subject.errors.messages).to include(
+          start_at: ['must not be nil End at']
+        )
+      end
+    end
+  end
+end
