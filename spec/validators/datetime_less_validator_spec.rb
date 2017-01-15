a9143006a5871edb3bd2d7d31e28b9341ec5cbc9
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2017-01-16 01:30:25 +0900

    Add spec

--- spec/validators/datetime_less_validator_spec.rb
+++ spec/validators/datetime_less_validator_spec.rb
@@ -53,6 +53,40 @@ shared_context 'greater_than_or_equal_to' do
   end
 end
 
+shared_context 'multiple options' do
+  let(:start_at) { Time.now }
+  let(:end_at)   { start_at.tomorrow }
+
+  subject { dummy_class.new(start_at, end_at, checked_at) }
+
+  context 'valid' do
+    let(:checked_at) { start_at }
+    it { is_expected.to be_valid }
+  end
+
+  context 'less than' do
+    let(:checked_at) { start_at.yesterday }
+
+    example do
+      is_expected.to be_invalid
+      expect(subject.errors.messages).to include(
+        checked_at: ['はStart atより以後の日時にしてください']
+      )
+    end
+  end
+
+  context 'greater than' do
+    let(:checked_at) { end_at.tomorrow }
+
+    example do
+      is_expected.to be_invalid
+      expect(subject.errors.messages).to include(
+        checked_at: ['はEnd atより以前の日時にしてください']
+      )
+    end
+  end
+end
+
 describe DatetimeLessValidator do
   let(:dummy_class) do
     Struct.new(:start_at, :end_at) do
@@ -84,6 +118,26 @@ describe DatetimeLessValidator do
     end
 
     include_context 'greater_than_or_equal_to'
+  end
+
+  describe 'multiple options' do
+    let(:dummy_class) do
+      Struct.new(:start_at, :end_at, :checked_at) do
+        include ActiveModel::Validations
+
+        validates :checked_at, 
+          datetime_less: {
+            less_than_or_equal_to:    :end_at,
+            greater_than_or_equal_to: :start_at 
+          }
+
+        def self.name
+          'Dummy'
+        end
+      end
+    end
+
+    include_context 'multiple options'
   end
 
   describe 'type error' do
