14b9f35beb14094d5729aa5f039d5cf4dffae47b
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2017-01-15 17:53:33 +0900

    Add :after to options

--- app/validators/datetime_less_validator.rb
+++ app/validators/datetime_less_validator.rb
@@ -1,21 +1,59 @@
 class DatetimeLessValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    before_datetime = record.send(options[:before])
-    target_attr     = record.class.human_attribute_name options[:before]
+  CHECKS = {
+    before: :<=,
+    after:  :>=
+  }
 
-    if before_datetime.blank?
-      error   = :required_before_datetime
-      message = "must not be nil #{target_attr}"
-    elsif (before_datetime - value).to_i < 0
-      error   = :less_than_datetime
-      message = "must be before #{target_attr}"
+  ERRORS = {
+    before: :before_datetime,
+    after:  :after_datetime
+  }
+
+  def initialize(options)
+    super
+    @option_key = option_keys.first
+    @comparison_attr = options[@option_key]
+  end
+
+  def check_validity!
+    raise ArgumentError, 'Requires :before or :after option'      if !required_option_exist?
+    raise ArgumentError, 'Duplicate option of :before and :after' if duplicate_option?
+  end
+
+  def validate_each(record, attribute, value)
+    target_datetime  = record.send(@comparison_attr)
+    target_attr_name = record.class.human_attribute_name(@comparison_attr)
+
+    if target_datetime.blank?
+      error   = :target_blank
+      message = "#{target_attr_name} can't be blank"
+    else
+      unless value.send(CHECKS[@option_key], target_datetime)
+        error   = ERRORS[@option_key]
+        message = "must be #{@option_key.to_s} #{target_attr_name}"
+      end
     end
 
     record.errors.add(
-      attribute, error,
-      target_attr: target_attr,
+      attribute, 
+      error,
+      target_attr: target_attr_name,
       message:     message
     ) if error.present?
+  end
+
+  private
+
+  def option_keys
+    CHECKS.keys & options.keys
+  end
+
+  def required_option_exist?
+    option_keys.present?
+  end
+
+  def duplicate_option?
+    options.key?(:before) && options.key?(:after)
   end
 end
 
--- spec/validators/datetime_less_validator_spec.rb
+++ spec/validators/datetime_less_validator_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-shared_examples "validate before option" do  
+shared_examples "validate before option" do
   describe 'start_at' do
     let(:base_datetime) { Time.current }
 
@@ -39,31 +39,146 @@ shared_examples "validate before option" do
       example do
         is_expected.not_to be_valid
         expect(subject.errors.messages).to include(
-          start_at: ['must not be nil End at']
+          start_at: ["End at can't be blank"]
         )
       end
     end
   end
 end
 
-describe DatetimeLessValidator do
-  describe 'option' do
-    let(:dummy_class) do
-      Struct.new(:start_at, :end_at) do
-        include ActiveModel::Validations
+shared_examples "validate after option" do
+  describe 'end_at' do
+    let(:base_datetime) { Time.current }
 
-        validates :start_at,
-          datetime_less: {
-            before: :end_at
-          }
+    subject { dummy_class.new(start_at, end_at) }
 
-        def self.name
-          'Dummy'
-        end
+    context 'greater than start_at' do
+      let(:start_at) { base_datetime }
+      let(:end_at)   { base_datetime.tomorrow }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'equal to start_at' do
+      let(:start_at) { base_datetime }
+      let(:end_at)   { base_datetime }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'less than start_at' do
+      let(:start_at) { base_datetime }
+      let(:end_at)   { base_datetime.yesterday }
+
+      example do
+        is_expected.not_to be_valid
+        expect(subject.errors.messages).to include(
+          end_at: ['must be after Start at']
+        )
       end
     end
 
-    include_examples "validate before option"
+    context 'target nil' do
+      let(:start_at) { nil }
+      let(:end_at)   { base_datetime }
+
+      example do
+        is_expected.not_to be_valid
+        expect(subject.errors.messages).to include(
+          end_at: ["Start at can't be blank"]
+        )
+      end
+    end
+  end
+end
+
+shared_examples "ArgumentError example" do |message|
+  subject { dummy_class.new(start_at, end_at) }
+
+  example do 
+    expect { subject.vaid? }.to raise_error(ArgumentError, message)
+  end
+end
+
+describe DatetimeLessValidator do
+  describe 'option' do
+    context 'before' do
+      let(:dummy_class) do
+        Struct.new(:start_at, :end_at) do
+          include ActiveModel::Validations
+
+          validates :start_at,
+            datetime_less: {
+              before: :end_at
+            }
+
+          def self.name
+            'Dummy'
+          end
+        end
+      end
+
+      include_examples "validate before option"
+    end
+
+    context 'after' do
+      let(:dummy_class) do
+        Struct.new(:start_at, :end_at) do
+          include ActiveModel::Validations
+
+          validates :end_at,
+            datetime_less: {
+              after: :start_at
+            }
+
+          def self.name
+            'Dummy'
+          end
+        end
+      end
+
+      include_examples "validate after option"
+    end
+
+    context 'argument error' do
+      context 'option not exists' do
+        let(:dummy_class) do
+          Struct.new(:start_at, :end_at) do
+            include ActiveModel::Validations
+
+            validates :end_at,
+              datetime_less: {
+              }
+
+            def self.name
+              'Dummy'
+            end
+          end
+        end
+
+        include_examples 'ArgumentError example', /Requires :before or :after optio/
+      end
+
+      context 'dupulicate option' do
+        let(:dummy_class) do
+          Struct.new(:start_at, :end_at) do
+            include ActiveModel::Validations
+
+            validates :end_at,
+              datetime_less: {
+                before: :start_at,
+                after:  :start_at
+              }
+
+            def self.name
+              'Dummy'
+            end
+          end
+        end
+
+        include_examples 'ArgumentError example', /Duplicate option of :before and :afte/
+      end
+    end
   end
 
   describe 'helper method' do
