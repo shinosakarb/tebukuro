3a948efb3086fefef37579656fa0d54dd40ae35f
chimame <rito.tamata@gmail.com>
2017-02-24 15:53:40 +0900

    Fix delete semicolons

--- front/__mocks__/fileTransformer.js
+++ front/__mocks__/fileTransformer.js
@@ -1,9 +1,9 @@
 // fileTransformer.js
 
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   process(src, filename, config, options) {
-    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
+    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';'
   },
-};
+}
