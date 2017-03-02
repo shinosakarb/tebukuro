341b4f1de4b0a251d433d98099f75f4f0fbd7ce9
Aki <aki.onetime@gmail.com>
2017-03-02 12:01:53 +0900

    implement #141. add event address

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,10 +6,12 @@ describe Event do
     it { is_expected.to have_many(:tickets) }
   end
 
-  describe 'presence' do
+  describe 'validation' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_presence_of(:event_starts_at) }
     it { is_expected.to validate_presence_of(:event_ends_at) }
+    it { is_expected.to validate_length_of(:address).is_at_most(255) }
   end
+
 end
