8d1023ce50f00cb94c6d4dacdcdeadffb857b797
chimame <rito.tamata@gmail.com>
2017-03-03 10:28:19 +0900

    Add ticket model of front

--- front/client/models/Event.js
+++ front/client/models/Event.js
@@ -16,17 +16,4 @@ export default class Event extends EventRecord {
   setTickets(list) {
     return this.set('tickets', Set.of(...list))
   }
-
-  isEnd() {
-    // TODO It depends on the local machine
-    return (new Date()).getTime() > this.convertEventEndsAtToDate().getTime()
-  }
-
-  convertEventStartsAtToDate() {
-    return this.eventStartsAt && this.eventStartsAt.length > 0 ? new Date(this.eventStartsAt) : null
-  }
-
-  convertEventEndsAtToDate() {
-    return this.eventEndsAt && this.eventEndsAt.length > 0 ? new Date(this.eventEndsAt) : null
-  }
 }
