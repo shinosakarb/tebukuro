1a6ab6ccd63990858f1b4901ecfd13fb83fe7ebb
ytorii <metalwarrior_type_jp@hotmail.com>
2017-03-21 09:55:14 +0900

    Add factories for test parameters

--- front/__tests__/containers/Event.spec.js
+++ front/__tests__/containers/Event.spec.js
@@ -4,36 +4,11 @@ import ToJson             from 'enzyme-to-json'
 import { Event }          from '../../client/containers/Event'
 import EventModel         from '../../client/models/Event'
 import TicketModel        from '../../client/models/Ticket'
+import EventParams        from '../factories/Event'
+import TicketParams       from '../factories/Ticket'
 
-const eventParams = {
-  id: 1,
-  name: 'test',
-  description: 'This event is active in Umeda.',
-  eventStartsAt: '2017-03-04T13:00:00.000Z',
-  eventEndsAt: '2017-03-04T17:00:00.000Z',
-  address: 'Hilton Plaza West 12F',
-}
-
-const ticketsParams = [ 
-  {
-    id:            3,
-    name:          'ticketName1',
-    cost:          4,
-    quantity:      5,
-    eventId:       1,
-    saleStartsAt: '2017-03-01T09:00:00.000Z',
-    saleEndsAt:   '2017-03-01T10:30:00.000Z'
-  },
-  {
-    id:            7,
-    name:          'ticketName2',
-    cost:          8,
-    quantity:      9,
-    eventId:       1,
-    saleStartsAt: '2017-12-11T13:30:00.000Z',
-    saleEndsAt:   '2017-12-11T15:00:00.000Z'
-  }
-]
+const eventParams = EventParams.event1
+const ticketsParams = [ TicketParams.ticket1, TicketParams.ticket2 ]
 
 let eventModel = new EventModel(eventParams)
 const ticketListModel = ticketsParams.map((ticket) => new TicketModel(ticket))
---
+++ front/__tests__/factories/Ticket.js
@@ -0,0 +1,20 @@
+export default {
+  ticket1: {
+    id:            1,
+    name:          'ticketName1',
+    cost:          500,
+    quantity:      50,
+    eventId:       1,
+    saleStartsAt: '2017-03-01T09:00:00.000Z',
+    saleEndsAt:   '2017-03-01T10:30:00.000Z'
+  },
+  ticket2: {
+    id:            2,
+    name:          'ticketName2',
+    cost:          4000,
+    quantity:      20,
+    eventId:       1,
+    saleStartsAt: '2017-12-11T13:30:00.000Z',
+    saleEndsAt:   '2017-12-11T15:00:00.000Z'
+  }
+}
