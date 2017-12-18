4c605235f014fefafbfdd0519c4a22c0f6518ae0
ytorii <metalwarrior_type_jp@hotmail.com>
2017-12-18 21:55:28 +0900

    Add Participant DELETE endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :communities, shallow: true, :defaults => { :format => 'json' } do
+  resources :communities, shallow: true, defaults: { format: 'json' } do
     resources :events do
       resources :tickets do
         resources :subscriptions, only: :create
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   end
 
   resources :events do
-    resources :participants, only: :create
+    resources :participants, only: %i[create destroy]
   end
 
   namespace :subscription do
