bf73639a155487fc7fe9478b2ca92a3523e161aa
ytorii <metalwarrior_type_jp@hotmail.com>
2018-01-10 17:33:55 +0900

    Remove unnecessary specs

--- spec/controllers/subscription/tickets_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-describe Subscription::TicketsController, type: :controller do
-  it { is_expected.to use_before_action(:authenticate_user!) }
-end
--- spec/controllers/subscriptions_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-describe SubscriptionsController, type: :controller do
-  it { is_expected.to use_before_action(:authenticate_user!) }
-end
