56de55b6f0a0fa292007a3bcbb09ed07215be96b
ogom <ogom@outlook.com>
2018-05-10 22:30:25 +0900

    Add validate at cannot_join to participant model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,6 +4,14 @@ class Participant < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
+  validate :cannot_join
+
+  def cannot_join
+    unless event.within_deadline?
+      errors.add(:event_id, :cannot_join)
+    end
+  end
+
   def name
     user.name
   end
