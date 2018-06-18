32e3360a305978900a63e1aa0ed6aae1b58c366b
ytorii <metalwarrior_type_jp@hotmail.com>
2018-06-18 14:06:20 +0900

    Using association for serializing user_participant for event

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,13 +1,11 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :quota,
-    :event_starts_at, :event_ends_at, :user_participation
+  attributes :id, :name, :description, :quota, :event_starts_at, :event_ends_at
 
+  has_one :user_participation
   has_many :participants
 
   def user_participation
-    user_participant =
-      UserParticipant.new(event: object, current_user: current_user)
-    UserParticipantSerializer.new(user_participant).attributes
+    UserParticipant.new(event: object, current_user: current_user)
   end
 
   # TODO: communitiesモデルが作成されたら、GETのURLをattributesに加えます。
